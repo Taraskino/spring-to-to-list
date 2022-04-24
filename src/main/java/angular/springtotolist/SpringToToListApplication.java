92d958e9cec9e0e04ef856fdecf83482298f3f7e
muzychka <TarasMuzychka1989@gmail.com>
2022-04-24 09:53:37 +0200

    Initial commit

---
+++ src/main/java/angular/springtotolist/SpringToToListApplication.java
@@ -0,0 +1,13 @@
+package angular.springtotolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringToToListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringToToListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/angular/springtotolist/SpringToToListApplicationTests.java
@@ -0,0 +1,13 @@
+package angular.springtotolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringToToListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
